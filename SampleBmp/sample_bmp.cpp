eecff697e2ebb344c23d937c3f4c70c8aee37efb
Lyuxi8006 <hhackerbox@163.com>
2023-05-09 11:30:32 +0800

    mmm

--- SampleBmp/sample_bmp.cpp
+++ SampleBmp/sample_bmp.cpp
@@ -31,8 +31,8 @@ int main()
 	size_t width = 0;
 	size_t height = 0;
 	uint8_t* pixels = nullptr;
-	int res = read_bmpfile(r_file_name.c_str(), &width, &height, &pixels);
-	//int res = read_data(file_name.c_str(), &width, &height, &pixels);
+	//int res = read_bmpfile(r_file_name.c_str(), &width, &height, &pixels);
+	int res = read_data(file_name.c_str(), &width, &height, &pixels);
 	if (res == 0)
 	{
 		write_bmpfile(w_file_name.c_str(), width, height, pixels);
